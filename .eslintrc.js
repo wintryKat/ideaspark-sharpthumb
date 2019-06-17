84fd13d05b7febeb820d9de588d669c58bc51e23
Katherine Aurelia <katherine.aurelia@cubic.com>
2019-06-17 17:02:39 -0500

    A bit of modernization and style.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['cubic-innovation'],
+  env: {
+    browser: false,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  rules: {
+    'no-console': 'warn',
+  }
+};
